d2643ad5dedef73b62b77cbaf4d1430de0ca2a5d
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2016-12-19 13:26:55 +0100

    glDrawArraysInstancedBaseInstance test case

--- examples/00 Hello Triangle/main.c
+++ examples/00 Hello Triangle/main.c
@@ -10,9 +10,9 @@ i32 main()
   SDL_GLContext sdl_glcontext;
   
   gfWindow(&sdl_window, &sdl_glcontext, 0, 0, "App", 1280, 720, 4);
-  
-  gpu_buffer_t mesh = gfBufferCreate((gpu_buffer_t){.format = xyz_f32, .count = 3});
-  
+
+  gpu_buffer_t mesh = gfBufferCreate((gpu_buffer_t){.format = xyz_f32, .count = 6});
+
   mesh.as_vec3[0].x = -0.5f;
   mesh.as_vec3[0].y = -0.5f;
   mesh.as_vec3[0].z =  0.0f;
@@ -24,7 +24,19 @@ i32 main()
   mesh.as_vec3[2].x =  0.5f;
   mesh.as_vec3[2].y = -0.5f;
   mesh.as_vec3[2].z =  0.0f;
-  
+
+  mesh.as_vec3[3].x = -1.5f;
+  mesh.as_vec3[3].y = -0.5f;
+  mesh.as_vec3[3].z =  0.0f;
+
+  mesh.as_vec3[4].x =  0.0f;
+  mesh.as_vec3[4].y =  1.1f;
+  mesh.as_vec3[4].z =  0.0f;
+
+  mesh.as_vec3[5].x =  -0.5f;
+  mesh.as_vec3[5].y = -0.5f;
+  mesh.as_vec3[5].z =  0.0f;
+
   gpu_cmd_t cmd[1] = {};
   cmd[0].count = mesh.count;
   cmd[0].instance_count = 1;
@@ -64,6 +76,8 @@ i32 main()
   while(1)
   {
     glClear(CLEAR_COLOR);
+    cmd[0].first = 3;
+    cmd[0].count = 3;
     gfDraw(pp, countof(cmd), cmd);
     
     SDL_Event event;
